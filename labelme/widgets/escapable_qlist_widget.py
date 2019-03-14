0c4ee379d28e9e57dbb71f0598ea5c86ca869857
Mubashir Hanif <mubashirhanif93@gmail.com>
2019-03-14 02:36:26 +0100

    added auto assignment of labels

--- labelme/widgets/escapable_qlist_widget.py
+++ labelme/widgets/escapable_qlist_widget.py
@@ -4,6 +4,22 @@ from qtpy import QtWidgets
 
 class EscapableQListWidget(QtWidgets.QListWidget):
 
+    def __init__(self):
+        QtWidgets.QListWidget.__init__(self)
+        self.prevItem = None
+        self.itemSelectionChanged.connect(self.item_changed)
+        self.itemClicked.connect(self.item_click)
+        
     def keyPressEvent(self, event):
         if event.key() == Qt.Key_Escape:
+            self.clearSelection()
+    
+    def item_changed(self):
+        self.prevItem = None
+
+    def item_click(self, item):
+        if not self.prevItem:
+            self.prevItem = item
+        elif self.prevItem == item:
+            self.prevItem = None
             self.clearSelection()
